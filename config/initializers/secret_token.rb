5ec3a32134487e1da34b532640b13de63d027360
Andrew Mercando <andrew.mercando@gmail.com>
2013-03-08 22:42:08 -0500

    One Month Rails Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+OneMonthRails::Application.config.secret_token = '740b12582e8a744214557b1ba9be2241f747638cbb8d13c1a56388bf458985494fbf62289dc53acccfdd4a54231493d871d727275a0fc52e5764710df773b1c8'
